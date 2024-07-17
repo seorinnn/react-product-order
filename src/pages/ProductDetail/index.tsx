021a069bc0809dad33e64656a130f0cec257d84d
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-17 20:47:25 +0900

    feat : 구매할 상품 개수 선택 기능 구현

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Image, Text } from '@chakra-ui/react';
+import { AddIcon, MinusIcon } from '@chakra-ui/icons';
+import { Box, Button, IconButton, Image, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -19,6 +20,7 @@ const ProductDetailPage = () => {
   const navigate = useNavigate();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     const fetchProductDetail = async () => {
@@ -41,7 +43,11 @@ const ProductDetailPage = () => {
   if (loading) return <Text>Loading...</Text>;
   if (!product) return <Text>상품 정보를 가져오는 중 오류가 발생했습니다.</Text>;
 
-  console.log('Rendered product:', product); //추가
+  const handleQuantityChange = (change: number) => {
+    setQuantity((prevQuantity) => Math.max(1, prevQuantity + change));
+  };
+
+  const totalPrice = product.detail.price.sellingPrice * quantity;
 
   return (
     <Box display="flex" alignItems="center" mt="30" ml="30">
@@ -56,7 +62,26 @@ const ProductDetailPage = () => {
         <Text fontSize="xx-large" mt="10">
           {product.detail.price.sellingPrice}원
         </Text>
+        <Box display="flex" alignItems="center" mt="40">
+          <IconButton
+            aria-label="Decrease quantity"
+            icon={<MinusIcon />}
+            onClick={() => handleQuantityChange(-1)}
+          />
+          <Text mx="20">{quantity}</Text>
+          <IconButton
+            aria-label="Increase quantity"
+            icon={<AddIcon />}
+            onClick={() => handleQuantityChange(1)}
+          />
+        </Box>
+        <Text fontSize="xl" fontWeight="bold" my="20">
+          총 결제금액: {totalPrice}원
+        </Text>
         <Button
+          padding="10"
+          border="1px solid"
+          borderColor="gray.400"
           onClick={() => {
             //로그인 로직은 추후 수정
             const isLoggedIn = true;
