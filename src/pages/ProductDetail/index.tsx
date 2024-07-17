834ca28719d20d5deed399d57c308d43689036f4
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-17 20:26:29 +0900

    style : 상품 상세페이지 디자인 수정

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -26,7 +26,6 @@ const ProductDetailPage = () => {
         const response = await axios.get<Product>(
           `https://react-gift-mock-api-seorinnn.vercel.app/api/v1/products/${productId}/detail`,
         );
-        console.log('API Response:', response.data);
         setProduct(response.data);
       } catch (error) {
         console.error('Product not found, redirecting to main page');
@@ -37,7 +36,6 @@ const ProductDetailPage = () => {
     };
 
     fetchProductDetail();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [productId, navigate]);
 
   if (loading) return <Text>Loading...</Text>;
@@ -46,24 +44,32 @@ const ProductDetailPage = () => {
   console.log('Rendered product:', product); //추가
 
   return (
-    <Box>
-      <Image src={product.detail.imageURL} alt={product.detail.name} />
-      <Text>{product.detail.name}</Text>
-      <Text>{product.detail.price.sellingPrice}</Text>
-      {/* <Button onClick={() => navigate(`/product/${productId}/purchase`)}>구매하기</Button> */}
-      <Button
-        onClick={() => {
-          //로그인 로직은 추후 수정
-          const isLoggedIn = true;
-          if (!isLoggedIn) {
-            navigate('/login');
-          } else {
-            navigate(`/product/${productId}/gift`);
-          }
-        }}
-      >
-        나에게 선물하기
-      </Button>
+    <Box display="flex" alignItems="center" mt="30" ml="30">
+      <Image
+        src={product.detail.imageURL}
+        alt={product.detail.name}
+        boxSize="450px"
+        objectFit="cover"
+      />
+      <Box ml="20">
+        <Text fontSize="x-large">{product.detail.name}</Text>
+        <Text fontSize="xx-large" mt="10">
+          {product.detail.price.sellingPrice}원
+        </Text>
+        <Button
+          onClick={() => {
+            //로그인 로직은 추후 수정
+            const isLoggedIn = true;
+            if (!isLoggedIn) {
+              navigate('/login');
+            } else {
+              navigate(`/product/${productId}/gift`);
+            }
+          }}
+        >
+          나에게 선물하기
+        </Button>
+      </Box>
     </Box>
   );
 };
