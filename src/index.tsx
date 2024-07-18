ef879495731fa1b5821646a54692802d16d9882c
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-18 21:18:09 +0900

    chore : chakra-ui 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import '@/styles';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -8,6 +9,8 @@ import App from '@/App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
