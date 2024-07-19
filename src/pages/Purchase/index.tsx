366ce33a0835ca8e3d8e25380aac5f4c74ed9957
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-19 17:35:36 +0900

    feat : 상품 구매 validation 기능 구현

--- src/pages/Purchase/index.tsx
+++ src/pages/Purchase/index.tsx
@@ -3,6 +3,10 @@ import {
   Button,
   Checkbox,
   Flex,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
   Image,
   Input,
   Select,
@@ -26,43 +30,76 @@ const PurchasePage = () => {
   const [useReceipt, setUseReceipt] = useState(false);
   const [receiptType, setReceiptType] = useState('');
   const [receiptNumber, setReceiptNumber] = useState('');
+  const [errors, setErrors] = useState({
+    message: '',
+    receiptNumber: '',
+  });
 
   if (loading) return <Text>Loading...</Text>;
   if (!product) return <Text>상품 정보를 가져오는 중 오류가 발생했습니다.</Text>;
 
-  const handlePurchase = () => {
-    if (useReceipt && !receiptNumber) {
-      // setShowAlert(true);
-      return;
+  //validation 기능
+  const validateForm = () => {
+    const newErrors = { message: '', receiptNumber: '' };
+
+    if (!message) {
+      newErrors.message = '메시지를 입력해주세요.';
+    } else if (message.length > 100) {
+      newErrors.message = '메시지는 100자 이내로 입력해주세요.';
     }
-    // 결제 로직 추후에 추가
-    console.log('결제 정보:', {
-      productId,
-      message,
-      quantity,
-      useReceipt,
-      receiptType,
-      receiptNumber,
-      totalPrice,
-    });
+
+    if (useReceipt) {
+      if (!receiptNumber) {
+        newErrors.receiptNumber = '현금영수증 번호를 입력해주세요.';
+      } else if (!/^\d+$/.test(receiptNumber)) {
+        newErrors.receiptNumber = '현금영수증 번호는 숫자만 입력해주세요.';
+      }
+    }
+
+    setErrors(newErrors);
+    return Object.values(newErrors).every((error) => error === '');
+  };
+
+  const handlePurchase = () => {
+    if (!validateForm()) {
+      return;
+    } else {
+      alert('주문이 완료되었습니다.');
+      console.log('결제 정보:', {
+        productId,
+        message,
+        quantity,
+        useReceipt,
+        receiptType,
+        receiptNumber,
+        totalPrice,
+      });
+    }
   };
 
   return (
     <Box p="4" maxW="800px" mx="auto">
       <Flex direction={{ base: 'column', md: 'row' }} gap="4">
         <Box flex="1" mr="70">
-          <Text fontSize="xl" mt="10" mb="4">
-            나에게 주는 선물
-          </Text>
-          <Textarea
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder="선물과 함께 보낼 메시지를 적어보세요"
-            minWidth="400"
-            minHeight="100"
-            resize="none"
-            mb="4"
-          />
+          <FormControl isInvalid={!!errors.message}>
+            <FormLabel fontSize="xl" mt="10" mb="4">
+              나에게 주는 선물
+            </FormLabel>
+            <Textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="선물과 함께 보낼 메시지를 적어보세요"
+              minWidth="400"
+              minHeight="100"
+              resize="none"
+              mb="4"
+            />
+            {errors.message ? (
+              <FormErrorMessage>{errors.message}</FormErrorMessage>
+            ) : (
+              <FormHelperText>메시지는 100자 이내로 입력해주세요.</FormHelperText>
+            )}
+          </FormControl>
           <Text fontSize="xl" mt="25" mb="30">
             선물내역
           </Text>
@@ -81,14 +118,17 @@ const PurchasePage = () => {
           </Flex>
         </Box>
         <Box flex="1">
-          <Text fontSize="large" mt="10" mb="4">
-            결제 정보
-          </Text>
-          <Flex alignItems="center" mt="10" mb="4">
-            <Checkbox isChecked={useReceipt} onChange={(e) => setUseReceipt(e.target.checked)} />
-            <Text ml="2">현금영수증 신청</Text>
-          </Flex>
-          <Box mb="4">
+          <FormControl>
+            <FormLabel fontSize="large" mt="10" mb="4">
+              결제 정보
+            </FormLabel>
+            <Flex alignItems="center" mt="10" mb="4">
+              <Checkbox isChecked={useReceipt} onChange={(e) => setUseReceipt(e.target.checked)} />
+              <Text ml="2">현금영수증 신청</Text>
+            </Flex>
+          </FormControl>
+          <FormControl isInvalid={!!errors.receiptNumber}>
+            <FormLabel mt="5">현금영수증 유형 선택</FormLabel>
             <Select
               value={receiptType}
               onChange={(e) => setReceiptType(e.target.value)}
@@ -98,13 +138,15 @@ const PurchasePage = () => {
               <option value="personal">개인소득공제</option>
               <option value="business">사업자증빙용</option>
             </Select>
+
             <Input
               type="number"
               placeholder="(-없이) 숫자만 입력해주세요."
               value={receiptNumber}
               onChange={(e) => setReceiptNumber(e.target.value)}
             />
-          </Box>
+            {errors.receiptNumber && <FormErrorMessage>{errors.receiptNumber}</FormErrorMessage>}
+          </FormControl>
           <Text fontSize="lg" fontWeight="bold" mt="30" mb="10">
             최종 결제 금액: {totalPrice}원
           </Text>
